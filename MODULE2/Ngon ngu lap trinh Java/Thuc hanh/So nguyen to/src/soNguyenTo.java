772e1dc439c0e8311b6f948a01344b056e8147cd
Tran Phuc <huuphuc.0261993@gmail.com>
2019-10-11 10:34:04 +0700

    upload

---
+++ MODULE2/Ngon ngu lap trinh Java/Thuc hanh/So nguyen to/src/soNguyenTo.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class soNguyenTo {
+    public static void main(String[] args) {
+        System.out.println("Enter your number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if(number<2){
+            System.out.println(number + " is not a prime" );
+        } else if(number >=2){
+            int i=2;
+            boolean check = true;
+            while (i <= Math.sqrt(number)){
+                if(number%i==0){
+                    check = false;
+                    break;
+                }
+                i++;
+            }
+            if(check){
+                System.out.println(number + " is a prime");
+            } else {
+                System.out.println(number + " is not a prime");
+            }
+        }
+
+    }
+}
